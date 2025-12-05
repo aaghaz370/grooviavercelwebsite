b505f23b05063f2b43c14d5eba205380db8c962e
Rolex sir <aghazafzal8294@gmail.com>
2025-12-05 15:12:43 +0530

    Update goldenEraData.js

--- src/data/goldenEraData.js
+++ src/data/goldenEraData.js
@@ -4,21 +4,21 @@
 export const goldenEraPlaylists = [
   {
     id: "3379491",                 // JioSaavn playlist id
-    name: "Golden Era: 90s Romance",
+    name: "Best of 90s - Hindi",
     type: "playlist",
     tagLine: "90s Bollywood love hits",   // optional, UI me use kar sakte ho
     image: [
       {
         quality: "50x50",
-        url: "https://c.saavncdn.com/182/Dilwale-Dulhania-Le-Jayenge-Hindi-1995-50x50.jpg",
+        url: "https://c.saavncdn.com/editorial/BestOf90sHindi_20251125041813.jpg?bch=1764046111",
       },
       {
         quality: "150x150",
-        url: "https://c.saavncdn.com/182/Dilwale-Dulhania-Le-Jayenge-Hindi-1995-150x150.jpg",
+        url: "https://c.saavncdn.com/editorial/BestOf90sHindi_20251125041813.jpg?bch=1764046111",
       },
       {
         quality: "500x500",
-        url: "https://c.saavncdn.com/182/Dilwale-Dulhania-Le-Jayenge-Hindi-1995-500x500.jpg",
+        url: "https://c.saavncdn.com/editorial/BestOf90sHindi_20251125041813.jpg?bch=1764046111",
       },
     ],
     songCount: 10,
@@ -27,16 +27,76 @@ export const goldenEraPlaylists = [
   },
 
   {
-    id: "202020202",
-    name: "Golden 80s Blockbusters",
+    id: "158224184",
+    name: "Best Of Sad Songs: 90s",
     type: "playlist",
-    tagLine: "Evergreen Bollywood 80s",
+    tagLine: "Superhit Hearbreak Songs",
     image: [
-      { quality: "50x50", url: "https://c.saavncdn.com/...50x50.jpg" },
-      { quality: "150x150", url: "https://c.saavncdn.com/...150x150.jpg" },
-      { quality: "500x500", url: "https://c.saavncdn.com/...500x500.jpg" },
+      { quality: "50x50", url: "https://c.saavncdn.com/editorial/BestOfSadSongs-90s_20251125041857.jpg?bch=1764893194" },
+      { quality: "150x150", url: "https://c.saavncdn.com/editorial/BestOfSadSongs-90s_20251125041857.jpg?bch=1764893194" },
+      { quality: "500x500", url: "https://c.saavncdn.com/editorial/BestOfSadSongs-90s_20251125041857.jpg?bch=1764893194" },
     ],
-    songCount: 35,
+    songCount: 10,
+    language: "hindi",
+    explicitContent: false,
+  },
+
+  {
+    id: "159470188",
+    name: "90s Duets - Hindi",
+    type: "playlist",
+    tagLine: "Popular Romantic Duets",
+    image: [
+      { quality: "50x50", url: "https://c.saavncdn.com/editorial/90sDuetsHindi_20240906030959.jpg?bch=1762945173" },
+      { quality: "150x150", url: "https://c.saavncdn.com/editorial/90sDuetsHindi_20240906030959.jpg?bch=1762945173" },
+      { quality: "500x500", url: "https://c.saavncdn.com/editorial/90sDuetsHindi_20240906030959.jpg?bch=1762945173" },
+    ],
+    songCount: 10,
+    language: "hindi",
+    explicitContent: false,
+  },
+
+  {
+    id: "154546814",
+    name: "90s Romance - Hindi",
+    type: "playlist",
+    tagLine: "Superhit Romantic Music",
+    image: [
+      { quality: "50x50", url: "https://c.saavncdn.com/editorial/90sRomanceHindi_20250228084527.jpg?bch=1762945080" },
+      { quality: "150x150", url: "https://c.saavncdn.com/editorial/90sRomanceHindi_20250228084527.jpg?bch=1762945080" },
+      { quality: "500x500", url: "https://c.saavncdn.com/editorial/90sRomanceHindi_20250228084527.jpg?bch=1762945080" },
+    ],
+    songCount: 10,
+    language: "hindi",
+    explicitContent: false,
+  },
+
+  {
+    id: "829466184",
+    name: "Decade Of Heroes - 1990s",
+    type: "playlist",
+    tagLine: "Superhits Of The Heroes",
+    image: [
+      { quality: "50x50", url: "https://c.saavncdn.com/editorial/DecadeOfHeroes1990s_20240903100210.jpg?bch=1762945567" },
+      { quality: "150x150", url: "https://c.saavncdn.com/editorial/DecadeOfHeroes1990s_20240903100210.jpg?bch=1762945567" },
+      { quality: "500x500", url: "https://c.saavncdn.com/editorial/DecadeOfHeroes1990s_20240903100210.jpg?bch=1762945567" },
+    ],
+    songCount: 10,
+    language: "hindi",
+    explicitContent: false,
+  },
+
+  {
+    id: "1208889803",
+    name: "90s Evergreen Hits",
+    type: "playlist",
+    tagLine: "Evergreen 90s",
+    image: [
+      { quality: "50x50", url: "https://c.saavncdn.com/editorial/90sEvergreenHits_20241128053307.jpg?bch=1733465606" },
+      { quality: "150x150", url: "https://c.saavncdn.com/editorial/90sEvergreenHits_20241128053307.jpg?bch=1733465606" },
+      { quality: "500x500", url: "https://c.saavncdn.com/editorial/90sEvergreenHits_20241128053307.jpg?bch=1733465606" },
+    ],
+    songCount: 39,
     language: "hindi",
     explicitContent: false,
   },
